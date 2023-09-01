1a341091f7f22bb9de7daf9da9d2e83f84de2c41
Boris Capitanu <capitanu@illinois.edu>
2023-09-01 17:55:07 -0500

    Updated build spec

---
+++ README.md
@@ -0,0 +1,36 @@
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/htrc/Metadata-bibframe-enrich/ci.yml?branch=main)](https://github.com/htrc/Metadata-bibframe-enrich/actions/workflows/ci.yml)
+[![codecov](https://codecov.io/github/htrc/Metadata-bibframe-entities/graph/badge.svg?token=2y6GAtWfnP)](https://codecov.io/github/htrc/Metadata-bibframe-entities)
+[![GitHub release (latest SemVer including pre-releases)](https://img.shields.io/github/v/release/htrc/Metadata-bibframe-enrich?include_prereleases&sort=semver)](https://github.com/htrc/Metadata-bibframe-enrich/releases/latest)
+
+# Metadata-bibframe-enrich
+Used to enrich Bibframe with entities from external sources
+
+# Build
+* To generate a package that can be invoked via a shell script, run:  
+  `sbt stage`  
+  then find the result in `target/universal/stage/` folder.
+* To generate a distributable ZIP package, run:  
+  `sbt dist`  
+  then find the result in `target/universal/` folder.
+
+# Run
+```
+bibframe-enrich
+  -e, --entities  <FILE>      The path to the 'resolved' Bibframe entities
+  -l, --log-level  <LEVEL>    (Optional) The application log level; one of INFO,
+                              DEBUG, OFF (default = INFO)
+  -n, --num-partitions  <N>   (Optional) The number of partitions to split the
+                              input set of HT IDs into, for increased
+                              parallelism
+  -o, --output  <DIR>         Write the output to DIR
+  -s, --save-as-seq           (Optional) Saves the EF files as Hadoop sequence
+                              files
+      --spark-log  <FILE>     (Optional) Where to write logging output from
+                              Spark to
+  -h, --help                  Show help message
+  -v, --version               Show version of this program
+
+ trailing arguments:
+  input (required)   The path to the folder containing the Bibframe XML sequence
+                     files to process
+```
--- src/main/scala/org/hathitrust/htrc/tools/ef/metadata/bibframeenrich/Main.scala
+++ src/main/scala/org/hathitrust/htrc/tools/ef/metadata/bibframeenrich/Main.scala
@@ -2,21 +2,20 @@ package org.hathitrust.htrc.tools.ef.metadata.bibframeenrich
 
 import java.io.{File, PrintWriter}
 import java.nio.file.Files
-
 import com.gilt.gfc.time.Timer
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkConf
 import org.hathitrust.htrc.tools.ef.metadata.bibframeenrich.Helper.logger
 import org.hathitrust.htrc.tools.ef.metadata.bibframeenrich.models.EntityResult
-import org.hathitrust.htrc.tools.scala.io.IOUtils.using
 import org.hathitrust.htrc.tools.spark.errorhandling.ErrorAccumulator
 import org.hathitrust.htrc.tools.spark.errorhandling.RddExtensions._
+import org.hathitrust.htrc.tools.spark.utils.Helper.stopSparkAndExit
 import play.api.libs.json.Json
 
 import scala.io.{Codec, Source}
 import scala.language.reflectiveCalls
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success, Try, Using}
 import scala.xml.{Elem, XML}
 
 object Main {
@@ -27,85 +26,96 @@ object Main {
     val inputPath = conf.inputPath().toString
     val outputPath = conf.outputPath().toString
     val entitiesFile = conf.entitiesFile()
-
-    conf.outputPath().mkdirs()
+    val saveAsSeqFile = conf.saveAsSeqFile()
 
     // set up logging destination
-    conf.sparkLog.toOption match {
-      case Some(logFile) => System.setProperty("spark.logFile", logFile)
-      case None =>
-    }
+    conf.sparkLog.foreach(System.setProperty("spark.logFile", _))
     System.setProperty("logLevel", conf.logLevel().toUpperCase)
+
+    implicit val codec: Codec = Codec.UTF8
 
     // set up Spark context
     val sparkConf = new SparkConf()
     sparkConf.setAppName(appName)
     sparkConf.setIfMissing("spark.master", "local[*]")
+    val sparkMaster = sparkConf.get("spark.master")
 
     val spark = SparkSession.builder()
       .config(sparkConf)
       .getOrCreate()
 
-//    import spark.implicits._
-
-    implicit val sc: SparkContext = spark.sparkContext
-    implicit val codec: Codec = Codec.UTF8
+    val sc = spark.sparkContext
 
     val numPartitions = conf.numPartitions.getOrElse(sc.defaultMinPartitions)
 
-    logger.info("Starting...")
+    try {
+      logger.info("Starting...")
+      logger.info(s"Spark master: $sparkMaster")
 
-    // record start time
-    val t0 = System.nanoTime()
+      logger.info(s"Loading entities from $entitiesFile")
 
-    logger.info(s"Loading entities from $entitiesFile")
-    val entitiesBcast = {
-      val entitiesMap = using(Source.fromFile(entitiesFile)) { entitiesSource =>
-        val entityResults = entitiesSource.getLines().map(line => Json.parse(line).as[EntityResult])
-        entityResults.map(er => er.entity -> er.value).toMap
+      // record start time
+      val t0 = System.nanoTime()
+
+      conf.outputPath().mkdirs()
+
+      val entitiesBcast = {
+        val entitiesMap = Using.resource(Source.fromFile(entitiesFile)) { entitiesSource =>
+          val entityResults = entitiesSource.getLines().map(line => Json.parse(line).as[EntityResult])
+          entityResults.map(er => er.entity -> er.value).toMap
+        }
+        logger.info(f"Loaded ${entitiesMap.size}%,d entities")
+        sc.broadcast(entitiesMap)
       }
-      logger.info(f"Loaded ${entitiesMap.size}%,d entities")
-      sc.broadcast(entitiesMap)
-    }
 
-    val xmlParseErrorAccumulator = new ErrorAccumulator[(String, String), String](_._1)
-    val bibframeXmlRDD = sc
-      .sequenceFile[String, String](inputPath, minPartitions = numPartitions)
-      .tryMapValues(XML.loadString)(xmlParseErrorAccumulator)
+      val xmlParseErrorAccumulator = new ErrorAccumulator[(String, String), String](_._1)(sc)
+      val bibframeXmlRDD = sc
+        .sequenceFile[String, String](inputPath, minPartitions = numPartitions)
+        .tryMapValues(XML.loadString)(xmlParseErrorAccumulator)
 
-//    val xmlEnrichErrorAccumulator = new ErrorAccumulator[(String, Elem), String](_._1)
-//    val enrichedBibframeXmlRDD = bibframeXmlRDD
-//      .tryMapValues(Helper.enrichVolume(_, entitiesBcast.value).toString())(xmlEnrichErrorAccumulator)
+      //    val xmlEnrichErrorAccumulator = new ErrorAccumulator[(String, Elem), String](_._1)(sc)
+      //    val enrichedBibframeXmlRDD = bibframeXmlRDD
+      //      .tryMapValues(Helper.enrichVolume(_, entitiesBcast.value).toString())(xmlEnrichErrorAccumulator)
 
-    val enrichedBibframeXmlRDD = bibframeXmlRDD
+      val enrichedBibframeXmlRDD = bibframeXmlRDD
         .flatMap { case (k, e) => Try(Helper.enrichVolume(e, entitiesBcast.value)) match {
           case Success(result) => Some(k -> result.toString())
           case Failure(t) =>
             logger.error(s"enrichVolume: id=$k", t)
             None
-        }}
+        }
+        }
 
-//    enrichedBibframeXmlRDD.foreach { case (id, xml) =>
-//      val fname = id.replaceAllLiterally(":", "+").replaceAllLiterally("/", "=")
-//      using(new PrintWriter(new File(outputPath, fname + ".xml"), "UTF-8"))(_.print(xml))
-//    }
-    enrichedBibframeXmlRDD.saveAsSequenceFile(outputPath + "/enriched")
+      if (saveAsSeqFile)
+        enrichedBibframeXmlRDD.saveAsSequenceFile(outputPath + "/enriched")
+      else
+        enrichedBibframeXmlRDD.foreach { case (id, xml) =>
+          val fname = id.replace(":", "+").replace("/", "=")
+          Using.resource(new PrintWriter(new File(outputPath, fname + ".xml"), "UTF-8"))(_.print(xml))
+        }
 
-//    if (xmlParseErrorAccumulator.nonEmpty || xmlEnrichErrorAccumulator.nonEmpty)
-    if (xmlParseErrorAccumulator.nonEmpty)
-      logger.info("Writing error report(s)...")
+      //    if (xmlParseErrorAccumulator.nonEmpty || xmlEnrichErrorAccumulator.nonEmpty)
+      if (xmlParseErrorAccumulator.nonEmpty)
+        logger.info("Writing error report(s)...")
 
-    if (xmlParseErrorAccumulator.nonEmpty)
-      xmlParseErrorAccumulator.saveErrors(new Path(outputPath, "xmlparse_errors.txt"))
+      if (xmlParseErrorAccumulator.nonEmpty)
+        xmlParseErrorAccumulator.saveErrors(new Path(outputPath, "xmlparse_errors.txt"))
 
-//    if (xmlEnrichErrorAccumulator.nonEmpty)
-//      xmlEnrichErrorAccumulator.saveErrors(new Path(outputPath, "enrich_errors.txt"))
+      //    if (xmlEnrichErrorAccumulator.nonEmpty)
+      //      xmlEnrichErrorAccumulator.saveErrors(new Path(outputPath, "enrich_errors.txt"))
 
-    // record elapsed time and report it
-    val t1 = System.nanoTime()
-    val elapsed = t1 - t0
+      // record elapsed time and report it
+      val t1 = System.nanoTime()
+      val elapsed = t1 - t0
 
-    logger.info(f"All done in ${Timer.pretty(elapsed)}")
+      logger.info(f"All done in ${Timer.pretty(elapsed)}")
+    }
+    catch {
+      case e: Throwable =>
+        logger.error(s"Uncaught exception", e)
+        stopSparkAndExit(sc, exitCode = 500)
+    }
+
+    stopSparkAndExit(sc)
   }
-
 }
